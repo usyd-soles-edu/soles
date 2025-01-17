a6434b57b2eb890e58e78b2b4892d93d6b9053fd
Januar Harianto <januar.harianto@sydney.edu.au>
2025-01-17 11:11:09 +1100

    feat: add support for xlsx files

--- R/parse_gradescope.R
+++ R/parse_gradescope.R
@@ -1,12 +1,21 @@
 #' Parse Gradescope export file
 #'
-#' @param x Path to Gradescope export file
+#' @param x Path to Gradescope export file (CSV or XLSX format)
 #' @return A data frame containing parsed Gradescope data
 #' @importFrom readr read_csv
+#' @importFrom readxl read_xlsx
 #' @importFrom dplyr select
+#' @importFrom tools file_ext
 #' @export
 parse_gradescope <- function(x) {
-  df <- suppressWarnings(readr::read_csv(x, show_col_types = FALSE))
+  file_type <- tools::file_ext(x)
+
+  df <- switch(tolower(file_type),
+    "csv" = suppressWarnings(readr::read_csv(x, show_col_types = FALSE)),
+    "xlsx" = suppressWarnings(readxl::read_xlsx(x)),
+    stop("Unsupported file format. Please use CSV or XLSX file.")
+  )
+
   out <- df |>
     dplyr::select("First Name":"Submission Time")
   return(invisible(out))
